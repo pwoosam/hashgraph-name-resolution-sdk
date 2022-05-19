1f36dfbeb2b190a01a1ccb3c0f82bb3b343556e4
mamorite <taylor@piefi.io>
2022-05-19 17:21:58 -0600

    fix abi rel path

--- dist/manager.js
+++ dist/manager.js
@@ -4,7 +4,7 @@ exports.getManagerInfo = void 0;
 const sdk_1 = require("@hashgraph/sdk");
 const MANAGER_ID = '0.0.34853598';
 // const MANAGER_ABI = 'node_modules\\@piefi-platform\\hashgraph-names-sdk\\src\\contracts\\abi\\src_contracts_nsNodes_sol_NSNodeManager.abi';
-const MANAGER_ABI = '.\\contracts\\abi\\src_contracts_nsNodes_sol_NSNodeManager.abi';
+const MANAGER_ABI = '..\\src\\contracts\\abi\\src_contracts_nsNodes_sol_NSNodeManager.abi';
 const getManagerInfo = () => {
     const managerId = sdk_1.ContractId.fromString(MANAGER_ID);
     const abi = MANAGER_ABI;
