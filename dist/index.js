666d67b09674fb1623a23c8c016d47a3865bfeae
mamorite <taylor@piefi.io>
2022-05-18 20:12:50 -0600

    Added dist files to repo

---
+++ dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.printMsg = void 0;
+function printMsg() {
+    alert("Hello, World!");
+    console.log("This is a test");
+}
+exports.printMsg = printMsg;
