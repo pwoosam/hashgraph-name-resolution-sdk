bd76c5017e5cb36775fd462b7cba739bfbbb1c39
mamorite <taylor@piefi.io>
2022-05-23 21:05:30 -0600

    Deployed new manager/token

--- dist/config/constants.config.js
+++ dist/config/constants.config.js
@@ -2,7 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.MAX_GAS = exports.TOKEN_ID = exports.MANAGER_ABI = exports.MANAGER_ID = exports.CONFIRMATION_STATUS = void 0;
 exports.CONFIRMATION_STATUS = 1;
-exports.MANAGER_ID = '0.0.34853598';
+exports.MANAGER_ID = '0.0.34896365';
 exports.MANAGER_ABI = '../src/contracts/abi/src_contracts_nsNodes_sol_NSNodeManager.abi';
-exports.TOKEN_ID = '0.0.34853601';
+exports.TOKEN_ID = '0.0.34896368';
 exports.MAX_GAS = 4000000;
