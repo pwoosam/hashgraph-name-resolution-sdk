55cabf7ff0a589557c12eee8a5a2fe88bff79b0b
nostradaomus <nate@piefi.io>
2022-09-12 13:46:08 -0600

    Update Dependencies and README

--- dist/test/index.test.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const index_1 = require("../index");
-test('end to end behavior works', async () => {
-    const resolver = new index_1.Resolver('hedera_test', '98966f0ee7024a18bedb460b568cc13f');
-    await resolver.init();
-    const result = await resolver.resolveSLD('palacios.hbar');
-    console.log(result);
-});
